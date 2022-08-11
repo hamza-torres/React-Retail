cc1a08809ddd269c227688bc119bcd4025016262
hamza-torres <93662311+hamza-torres@users.noreply.github.com>
2022-08-11 15:49:59 +0200

    categories-page

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,4 +1,4 @@
-import CategoryItem from '../category-item/category-item.component'
+import DirectoryItem from '../directory-item/directory-item.component'
 import './directory.styles.scss';
 
 
@@ -34,7 +34,7 @@ const Directory = () => {
     return (
         <div className="categories-container">
             {categories.map(category => (
-                <CategoryItem category={category} key={category.id} />
+                <DirectoryItem category={category} key={category.id} />
             ))}
         </div>
     )
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -2,14 +2,23 @@
 import { initializeApp } from "firebase/app";
 import {
     getAuth,
-    signInWithPopup, 
-    GoogleAuthProvider, 
-    createUserWithEmailAndPassword, 
-    signInWithEmailAndPassword, 
+    signInWithPopup,
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
     signOut,
     onAuthStateChanged
 } from "firebase/auth";
-import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc,
+    collection,
+    writeBatch,
+    query,
+    getDocs
+} from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -37,6 +46,35 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
 
 
 export const db = getFirestore();
+
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = collection(db, collectionKey);
+    const batch = writeBatch(db);
+    objectsToAdd.forEach(obj => {
+        const newDocRef = doc(collectionRef, obj.title.toLowerCase());
+        batch.set(newDocRef, obj);
+    }
+    );
+    await batch.commit();
+}
+
+
+export const getCategoriesAndDocuments = async () => {
+    const collectionRef = collection(db, 'categories');
+    const q = query(collectionRef);
+    const querySnapshot = await getDocs(q);
+    const categoryMap = querySnapshot.docs.reduce((acc, doc) => {
+        const { title, items } = doc.data();
+        acc[title.toLowerCase()] = items;
+        return acc;
+    }
+    , {});
+    return categoryMap;
+}
+
+
+
 export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
     if (!userAuth) return;
 
